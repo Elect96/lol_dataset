a3666d13e582bbf1248eb79138c2c88b4c07c607
Jakub Lamczyk <lamczykjakub@gmail.com>
2020-07-01 13:18:06 +0100

    Initial commit

---
+++ champions.py
@@ -0,0 +1,7 @@
+# This file is meant to extract champions information from the current patch provided to it
+import os
+
+root = "D:\\Python\\League of Legends\\dragontail-10.13.1\\10.13.1\\data\\en_GB"
+
+# loop over champion files
+for
