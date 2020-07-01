0c3ff1b92cf85877b3777cbe3ef8fe31413674af
Jakub Lamczyk <lamczykjakub@gmail.com>
2020-07-01 18:22:39 +0100

    champions.py gets the data of all the champions and their stats from provided dataset and saves it to champions dict

--- champions.py
+++ champions.py
@@ -1,7 +1,25 @@
 # This file is meant to extract champions information from the current patch provided to it
 import os
+import json
 
-root = "D:\\Python\\League of Legends\\dragontail-10.13.1\\10.13.1\\data\\en_GB"
+# data path
+path = "D:\\Python\\League of Legends\\dragontail-10.13.1\\10.13.1\\data\\en_GB\\champion\\"
+# dictionary; "champion": stats
+champions = dict()
 
-# loop over champion files
-for
+# loop over the whole folder tree in the given path
+for dirs, subdirs, files in os.walk(path):
+    # loop over every file
+    for file in files:
+        # if the file exists do following
+        if file:
+            # open that file and save it to data
+            with open(path + file, "r", encoding="utf-8") as f:
+                data = json.load(f)
+            # extract a champion's name from a file's name
+            champion_name = file.split(".")[0]
+            # save every champion to a dictionary
+            champions[champion_name] = data.get("data").get(champion_name).get("stats")
+
+# print(champions)
+# TODO: save champions into a text file
